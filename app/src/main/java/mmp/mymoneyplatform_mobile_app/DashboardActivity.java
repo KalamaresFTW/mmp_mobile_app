7f9ed58af50b59ee9a480e28bc260992f700fb7b
nachosorocks <nachosines@gmail.com>
2016-04-11 11:42:11 +0100

    Fixed little bugs

--- app/src/main/java/mmp/mymoneyplatform_mobile_app/DashboardActivity.java
+++ app/src/main/java/mmp/mymoneyplatform_mobile_app/DashboardActivity.java
@@ -1,28 +1,35 @@
 package mmp.mymoneyplatform_mobile_app;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageView iv_menu_img;
+    private TextView tv_menu_name, tv_menu_mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        initComponents();
+        loadProfileData();
+    }
+
+    public void initComponents() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -32,6 +39,17 @@ public class DashboardActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Initialisation of the Navigator Menu´s components
+        iv_menu_img = (ImageView) findViewById(R.id.iv_menu_img);
+        tv_menu_name = (TextView) findViewById(R.id.tv_menu_name);
+        tv_menu_mail = (TextView) findViewById(R.id.tv_menu_mail);
+
+    }
+
+    public void loadProfileData() {
+        tv_menu_name.setText("Sean McNulty");
+        tv_menu_mail.setText("SeanMcNulty@gmail.com");
     }
 
     @Override
@@ -62,7 +80,7 @@ public class DashboardActivity extends AppCompatActivity
 
                 break;
             case R.id.nav_logout:
-
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 break;
             default:
                 return false;
