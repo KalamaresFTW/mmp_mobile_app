016b1764939898e356e6cccb7d113ba799339d25
nachosorocks <nachosines@gmail.com>
2016-04-15 12:34:45 +0100

    Added Calendar EditText

--- app/src/main/java/mmp/mymoneyplatform_mobile_app/activity/RegisterActivity.java
+++ app/src/main/java/mmp/mymoneyplatform_mobile_app/activity/RegisterActivity.java
@@ -1,12 +1,25 @@
 package mmp.mymoneyplatform_mobile_app.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
 import mmp.mymoneyplatform_mobile_app.R;
 import mmp.mymoneyplatform_mobile_app.util.FontsOverride;
 
 public class RegisterActivity extends AppCompatActivity{
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private EditText dateOfBirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,39 @@ public class RegisterActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        dateOfBirth = (EditText) findViewById(R.id.reg_dateofbirth);
+        calendar = Calendar.getInstance();
+    }
 
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "Calendar", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateOfBirth.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 }
