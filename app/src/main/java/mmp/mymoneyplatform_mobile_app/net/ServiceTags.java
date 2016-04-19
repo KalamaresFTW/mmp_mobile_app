bf6ac14fe2b45a6f9cb5b1d3fdec543ff22a1c77
Pablo <Pablo.kmba2@gmail.com>
2016-04-19 12:41:45 +0100

    Code cleanup and a few refactors here and there, login activity should be now a bunker

---
+++ app/src/main/java/mmp/mymoneyplatform_mobile_app/net/ServiceTags.java
@@ -0,0 +1,16 @@
+package mmp.mymoneyplatform_mobile_app.net;
+
+/**
+ * Created by K on 19/04/2016.
+ */
+public class ServiceTags {
+
+    //Tags to retrieve the information from the JSON object
+    public static final String SUBSCRIPTIONID_TAG = "userSubscriptionID";
+    public static final String REGION_TAG = "region";
+    public static final String PROFILEIMAGE_TAG = "profileImage";
+    public static final String COUNTRY_TAG = "country";
+    public static final String NEWUSER_TAG = "newUser";
+    public static final String NAME_TAG = "Name";
+
+}
