7300cb620710a31f23d75508de11ed48f47d05c4
Pablo <Pablo.kmba2@gmail.com>
2016-04-21 14:48:57 +0100

    Fixed custom spinners layout

--- app/src/main/java/mmp/mymoneyplatform_mobile_app/activity/ProfileActivity.java
+++ app/src/main/java/mmp/mymoneyplatform_mobile_app/activity/ProfileActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.graphics.Region;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,10 +22,26 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import mmp.mymoneyplatform_mobile_app.R;
+import mmp.mymoneyplatform_mobile_app.net.ServiceTags;
+import mmp.mymoneyplatform_mobile_app.net.ServiceURL;
+import mmp.mymoneyplatform_mobile_app.pojo.FrecuencyData;
 import mmp.mymoneyplatform_mobile_app.pojo.RegionData;
 import mmp.mymoneyplatform_mobile_app.pojo.UserData;
 import mmp.mymoneyplatform_mobile_app.util.FontsOverride;
@@ -45,9 +62,19 @@ public class ProfileActivity extends AppCompatActivity {
     //State Switcher (Modify mode or block mode)
     private boolean isModifying = false;
 
+    private PaidFrequencyLoader mPaidFrequencyLoaderTask;
+    private RegionDataLoader mRegionDataLoaderTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mPaidFrequencyLoaderTask = new PaidFrequencyLoader();
+        mPaidFrequencyLoaderTask.execute((Void) null);
+
+        mRegionDataLoaderTask = new RegionDataLoader();
+        mRegionDataLoaderTask.execute((Void) null);
+
         //Set the new font
         FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/Raleway-Regular.ttf");
         //Set the layout
@@ -175,5 +202,129 @@ public class ProfileActivity extends AppCompatActivity {
     public void sendDataToTheApiLel() {
         Toast.makeText(getApplicationContext(), "Lol as sido altamente trolliado xDDDDDDDD", Toast.LENGTH_LONG).show();
     }
-    //endregion
+
+    class PaidFrequencyLoader extends AsyncTask<Void, Void, Void> {
+
+        private ArrayList<FrecuencyData> frecuencyData = new ArrayList<>();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String param;
+            URL url = null;
+            try {
+                param = "entityName=" + URLEncoder.encode(ServiceURL.URL_PARAM_PAYPERIOD, "UTF-8");
+                url = new URL(ServiceURL.DEFAULT + "?" + param);
+            } catch (MalformedURLException | UnsupportedEncodingException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(url);
+
+            HttpURLConnection urlConnection = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+            } catch (IOException ex) {
+                System.err.println("Error: " + ex.getMessage());
+            }
+            try {
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder response = new StringBuilder();
+                String partialResponse;
+                while ((partialResponse = bufferedReader.readLine()) != null) {
+                    String cleanString = partialResponse.replaceAll("(\\\\r\\\\n|\\\\n|\\\\)", "");
+                    response.append(cleanString);
+                }
+                bufferedReader.close();
+                String json = response.toString();
+                JSONObject JSONResponse;
+                JSONArray JSONArray;
+                try {
+                    JSONArray = new JSONArray(json.substring(json.indexOf("["), json.lastIndexOf("]") + 1));
+                    for (int i = 0; i < JSONArray.length(); i++) {
+                        JSONResponse = JSONArray.getJSONObject(i);
+                        frecuencyData.add(new FrecuencyData(
+                                JSONResponse.getInt(ServiceTags.PAYPERIODID_TAG),
+                                JSONResponse.getString(ServiceTags.PAYPERIOD_TAG)
+                        ));
+                    }
+                } catch (JSONException e) {
+                    System.err.println("Error: " + e.getMessage());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            ArrayAdapter<FrecuencyData> adapter = new ArrayAdapter<>(getApplicationContext(),
+                    android.R.layout.simple_list_item_1, frecuencyData);
+            mPaymentSpinner.setAdapter(adapter);
+        }
+    }
+    class RegionDataLoader extends AsyncTask<Void, Void, Void> {
+
+        private ArrayList<RegionData> countryList = new ArrayList<>();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String param;
+            URL url = null;
+            try {
+                param = "entityName=" + URLEncoder.encode(ServiceURL.URL_PARAM_COUNTRY, "UTF-8");
+                url = new URL(ServiceURL.DEFAULT + "?" + param);
+            } catch (MalformedURLException | UnsupportedEncodingException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(url);
+
+            HttpURLConnection urlConnection = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+            } catch (IOException ex) {
+                System.err.println("Error: " + ex.getMessage());
+            }
+            try {
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder response = new StringBuilder();
+                String partialResponse;
+                while ((partialResponse = bufferedReader.readLine()) != null) {
+                    String cleanString = partialResponse.replaceAll("(\\\\r\\\\n|\\\\n|\\\\)", "");
+                    response.append(cleanString);
+                }
+                bufferedReader.close();
+                String json = response.toString();
+                JSONObject JSONResponse;
+                JSONArray JSONArray;
+                try {
+                    JSONArray = new JSONArray(json.substring(json.indexOf("["), json.lastIndexOf("]") + 1));
+                    for (int i = 0; i < JSONArray.length(); i++) {
+                        JSONResponse = JSONArray.getJSONObject(i);
+                        countryList.add(new RegionData(
+                                JSONResponse.getInt(ServiceTags.JURISDICTIONID_TAG),
+                                JSONResponse.getString(ServiceTags.JURISDICTION_TAG)
+                        ));
+                    }
+                } catch (JSONException e) {
+                    System.err.println("Error: " + e.getMessage());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            ArrayAdapter<RegionData> adapter = new ArrayAdapter<>(getApplicationContext(),
+                    android.R.layout.simple_list_item_1, countryList);
+            mRegionSpinner.setAdapter(adapter);
+        }
+    }
 }
