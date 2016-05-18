58c0758c5798954cde1bf9377078006878499eb8
Pablo <Pablo.kmba2@gmail.com>
2016-05-18 12:51:59 +0100

    Added all the stuff for the assets/debts screen, still needs work

--- app/src/main/java/mmp/mymoneyplatform_mobile_app/fragment/assetsdebts/AssetsDebtsInputsFragment.java
+++ app/src/main/java/mmp/mymoneyplatform_mobile_app/fragment/assetsdebts/AssetsDebtsInputsFragment.java
@@ -5,10 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import mmp.mymoneyplatform_mobile_app.R;
 
 public class AssetsDebtsInputsFragment extends Fragment {
+
+
+    ImageButton mLoanCalculator, mMortageCalculator; //Calculators buttons
+    ImageButton mCalculatorsInfo, mAssetsDebtsListInfo; //Info buttons
+
+
+
     public AssetsDebtsInputsFragment() {
     }
 
@@ -20,8 +32,50 @@ public class AssetsDebtsInputsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_assetsdebts_inputs, container, false);
+        View view = inflater.inflate(R.layout.fragment_assetsdebts_inputs, container, false);
+
+        //Info Buttons
+        mCalculatorsInfo = (ImageButton) view.findViewById(R.id.btn_assets_debts_calculators_info);
+        mCalculatorsInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(getContext())
+                        .title(getString(R.string.info_dialog_title))
+                        .content(getString(R.string.assets_debts_screen_calculators_info))
+                        .positiveText(getString(R.string.confirm))
+                        .show();
+            }
+        });
+        mAssetsDebtsListInfo = (ImageButton) view.findViewById(R.id.btn_assets_debts_list_info);
+        mAssetsDebtsListInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(getContext())
+                        .title(getString(R.string.info_dialog_title))
+                        .content(getString(R.string.assets_debts_screen_list_info))
+                        .positiveText(getString(R.string.confirm))
+                        .show();
+            }
+        });
+
+
+        mLoanCalculator = (ImageButton) view.findViewById(R.id.btn_loan_calculator);
+        mLoanCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Loan Calculator", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mMortageCalculator = (ImageButton) view.findViewById(R.id.btn_mortage_calculator);
+        mMortageCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Mortage Calculator", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
     }
 
 
